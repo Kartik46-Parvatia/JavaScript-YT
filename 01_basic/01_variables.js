795acc96a7df1e9b63c239ed1eb0c330fdd7ebbc
Kartik46-Parvatia <parvatiakartik8@gmail.com>
2026-09-24 00:35:28 +0530

    Made and worte codes in 01_variables.js file

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const account_id = 144553
+let account_email = "kartik@gmail.com"
+var account_pass = "12345"
+account_city = "Jaipur"
+let account_state;
+
+// account_id = 2 // not allowed
+
+account_email = "parvatia@gmail.com"
+account_pass = "121212"
+account_city = "Dahod"
+
+console.log(account_id);
+
+
+/*
+prefet not to use var
+bcoz of issue in block scope and functional scope
+*/
+
+
+
+console.table([account_id, account_email, account_pass, account_city, account_state])
